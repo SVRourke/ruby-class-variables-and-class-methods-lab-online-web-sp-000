ea2ab9722c80926184438edfc1be1a280094c014
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-15 20:52:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,9 +7,9 @@ class Song
   def initialize(name, artist, genre)
     @name = name,
     @artist = artist,
-    @genre = genre,
-    @@count += 1,
-    @@genres << genre,
+    @genre = genre
+    @@count += 1
+    @@genres << genre
     @@artists << artist
   end
 
